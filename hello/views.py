19fb4cdd0a2522b78844a12aa28f14fab444ecd0
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2026-05-20 17:55:56 +0900

    gha cicd test

---
+++ hello/views.py
@@ -0,0 +1,11 @@
+# from django.shortcuts import render
+from django.http import JsonResponse
+import os, socket, datetime
+
+def index(request):
+    return JsonResponse({
+        "message": "hello from github actions cicd",
+        "host": socket.gethostname(),
+        "deployed_at_env": os.environ.get("DEPLOYED_AT", "unknown"),
+        "server_time": datetime.datetime.now().isoformat(),
+    })
